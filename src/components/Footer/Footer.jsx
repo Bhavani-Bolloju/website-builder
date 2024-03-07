f960439536706673a5d6e892729ba499e81ae801
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2024-03-07 13:02:36 +0530

    style footer and header part

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,32 @@
 import classes from "./Footer.module.scss";
 
 function Footer() {
-  return <div className={classes.footer}>footer</div>;
+  return (
+    <footer className={classes.footer}>
+      <div className={classes["footer__container"]}>
+        <div className={classes["footer__links"]}>
+          <div className={classes["footer__title"]}>catergories</div>
+          <ul>
+            <li>website builder</li>
+            <li>hosting</li>
+            <li>data center</li>
+            <li>robotic-automation</li>
+          </ul>
+        </div>
+        <div className={classes["footer__links"]}>
+          <div className={classes["footer__title"]}>contact</div>
+          <ul>
+            <li>contact</li>
+            <li>private policy</li>
+            <li>terms of service</li>
+            <li>catergories</li>
+            <li>about</li>
+          </ul>
+        </div>
+        <div>united states</div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header/HeaderNav.jsx
+++ src/components/Header/HeaderNav.jsx
@@ -1,8 +1,45 @@
 // import React from "react";
 import classes from "./HeaderNav.module.scss";
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+// import InputGroup from "react-bootstrap/InputGroup";
 
 function HeaderNav() {
-  return <nav className={classes.nav}>header nav</nav>;
+  return (
+    <header className={classes.header}>
+      <div className={classes["header__search"]}>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </button>
+        <input type="search" id="search" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#categories">categories</a>
+          </li>
+          <li>
+            <a href="#builders">website builders</a>
+          </li>
+          <li>
+            <a href="#deals">Today&#39;s deals</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default HeaderNav;
